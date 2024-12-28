816ed07e7f368e7a146e9c8de592e65a59888838
AsmitNepali <33512152+AsmitNepali@users.noreply.github.com>
2024-12-28 18:30:55 +0545

    ADD: New project.

---
+++ resources/views/components/welcome.blade.php
@@ -0,0 +1,15 @@
+<div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+    <h1 class="mt-8 text-2xl font-medium text-gray-900">
+        Welcome to the Laraconf
+    </h1>
+
+    <div class="mt-6 leading-relaxed">
+        <ol>
+            <li>1. Introduction of laravel</li>
+            <li>2. Laravel architecture</li>
+            <li>3. Why Laravel ?</li>
+            <li>4. What laravel comes with ?</li>
+            <li>5. Build a simple CURD base ticket system ?</li>
+        </ol>
+    </div>
+</div>
